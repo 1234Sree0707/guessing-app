aecbce6e50b30d295513910ab3604b67c2c4e828
1234Sree0707 <sreerammpm@gmail.com>
2026-02-23 14:16:39 +0530

    Added user guess submission where user enters a value and it checks if it is correct value or not

--- GuessingApp.java
+++ GuessingApp.java
@@ -9,11 +9,24 @@
 *@author Developer
 *@version 1.0
 */
+import java.util.Scanner;
 public class GuessingApp{	
 	public static void main(String[] args){
 		System.out.println("Welcome to the Guessing App");
 		GameConfig gameConfig=new GameConfig();
 		gameConfig.showRules();
+		Scanner sc=new Scanner(System.in);
+		int attempts=0;
+		while(attempts<gameConfig.getMaxAttemps()){
+			System.out.println("Enter your guess: ");
+			int guess=sc.nextInt();
+			attempts++;
+			String result=GuessValidator.validateGuess(guess, gameConfig.getTargetNumber());
+			System.out.println(result);
+			if("CORRECT".equals(result)){
+				break;
+			}
+        }			
 	}
 }	
 		
